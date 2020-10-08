cec9237db2b732d25c5645f3f09fe171f6f896f7
damianN <damian@bankingofthings.io>
2020-10-08 12:58:02 +0200

    compiles

--- src/MessagesService.cpp
+++ src/MessagesService.cpp
@@ -5,48 +5,72 @@
     The CORE expects a trigger action from IoT after the message is received.
     To finish the process, after custom actions (read sensors and do actuator stuff), triggerAction with actionID and pass the customerID as alternativeID (aid)
 
-  MessagesService.h - Class and Methods to get messages (bottalk) with BoT Service
-  Created by Ercan Bozoglu, September 30, 2012.
-  Released into the repository BoT-ESP32-SDK.
-*/
+   MessagesService.h - Class and Methods to get messages (bottalk) with BoT Service
+   Created by Ercan Bozoglu, September 30, 2012.
+   Released into the repository BoT-ESP32-SDK.
+ */
 
 #include "MessagesService.h"
 
-  /**
-   * Payload structure
+/**
+ * Payload structure
    data class MessagePojo(
-           val deviceID: String,
-           val payload: String,
-           val messageID: String,
-           val event: String?,
-           val delivered: Int
-                         ) {
-       var payloadModel: PayloadModel? = null
+         val deviceID: String,
+         val payload: String,
+         val messageID: String,
+         val event: String?,
+         val delivered: Int
+                       ) {
+     var payloadModel: PayloadModel? = null
 
-       data class PayloadModel(
-               val actionID: String?,
-               val customerID: String?,
-               val deviceID: String?
-                              )
+     data class PayloadModel(
+             val actionID: String?,
+             val customerID: String?,
+             val deviceID: String?
+                            )
    }
-   **/
+ **/
 MessagesService :: MessagesService(){
-  bot = BoTService :: getBoTServiceInstance();
+        bot = BoTService :: getBoTServiceInstance();
 }
-    /**
-     * @returns pair with actionID and customerID
-     * TODO in the future, CORE is going to send list of messages (queued). Now it expects object, single message.
-     */
-    std::pair MessagesService :: getMessages(){
-        String* response = bot->get(MESSAGES_END_POINT);
-        debugD("\nMessagesService :: getMessages : %s", response->c_str());
-        DynamicJsonBuffer jsonBuffer;
-        JsonObject& messagePayload = jsonBuffer.parseObject(receivedString->c_str());
-        JsonObject& payloadPayload = jsonBuffer.parseObject(messagePayload.get<const char*>("payload"));
+/**
+ * @returns pair with actionID and customerID
+ * TODO in the future, CORE is going to send list of messages (queued). Now it expects object, single message.
+ */
+String* MessagesService :: getMessages(){
+        String* messages = bot->get(MESSAGES_END_POINT);
+        debugD("\nMessagesService :: getMessages : %s", messages->c_str());
+        if(messages->indexOf("[") != -1 && messages->indexOf("]") != -1) {
+                DynamicJsonBuffer jsonBuffer;
+                JsonArray& messagesArray = jsonBuffer.parseArray(*messages);
+                if(messagesArray.success()) {
+                        int messagesCount = messagesArray.size();
+                        debugD("\MessagesService :: getMessages: JSON Messages array parsed successfully");
+                        debugD("\MessagesService :: getMessages: Number of messages returned: %d", messagesCount);
 
-        String* actionID = new String(payloadPayload.get<const char*>("actionID"));
-        String* customerID = new String(messagePayload.get<const char*>("customerID"));
-        // TODO replace pair with string array if not supported.
-        return std::make_pair(actionID, customerID);
-    }
+                        //Process the new set of messages
+                        for(byte i=0; i < messagesCount; i++) {
+                                const char* actionID = messagesArray[i]["actionID"];
+                                const char* customerID = messagesArray[i]["customerID"];
+                                const char* deviceID = messagesArray[i]["deviceID"];
+                                const char* event = messagesArray[i]["event"];
+                                debugD("\nID: action: %s customer: %s device: %s event: %s", actionID, customerID, deviceID, event);
+                        }
+                        debugI("\nActionService :: getMessages: Added %d messages returned from server ");
+                        return messages;
+                }
+                else {
+                        debugE("\MessagesService :: getMessages: JSON Messages array parsed failed!");
+                        debugW("\MessagesService :: getMessages: use locally stored actions, if available");
+                        debugW("\nActionService :: getMessages: Local messages count: %d", messagesArray.size());
+                        return NULL;
+                }
+        }
+        else {
+                debugE("\MessagesService :: getMessages: Could not retrieve messages from server");
+                debugW("\MessagesService :: getMessages: use locally stored messages, if available");
+
+                return NULL;
+        }
 }
+
